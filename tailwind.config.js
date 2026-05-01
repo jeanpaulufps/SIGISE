3a5f8b34976a34131bc8680d3db6405053f7e421
JeanP78 <jeanp0478@gmail.com>
2026-05-01 12:21:15 -0500

    tailwind added and configurated

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#16a34a',   // verde del logo
+        secondary: '#0f172a', // azul oscuro
+        accent: '#facc15',    // amarillo
+      },
+    },
+  },
+  plugins: [],
+}
