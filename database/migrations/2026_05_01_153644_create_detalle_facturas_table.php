ec0d4fa8a5307af5051de683605a653294648bf1
JeanP78 <jeanp0478@gmail.com>
2026-05-01 10:40:49 -0500

    migrations

---
+++ database/migrations/2026_05_01_153644_create_detalle_facturas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facturas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('deportista_id')->constrained()->onDelete('cascade');
+            $table->decimal('total_pagado', 10, 2);
+            $table->date('fecha');
+            $table->boolean('paz_y_salvo')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('detalle_facturas');
+    }
+};
